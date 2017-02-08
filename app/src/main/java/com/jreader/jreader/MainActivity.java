c9044b9cf189d4325e291093d9283ebd93e725b0
chenshaopeng <chenshaopeng90@hotmail.com>
2017-02-08 18:06:06 +0800

    sistatus bar

--- app/src/main/java/com/jreader/jreader/MainActivity.java
+++ app/src/main/java/com/jreader/jreader/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jreader.jreader;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
     }
 }
